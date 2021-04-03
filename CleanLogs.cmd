7f405f3f9d08ff8729cdd1e9b7796365bb10d77e
TomBitDe <tbit455674@gmail.com>
2021-04-03 11:43:37 +0200

    Corrected.

--- CleanLogs.cmd
+++ CleanLogs.cmd
@@ -5,5 +5,11 @@ echo=
 del %PAYARA_HOME%\glassfish\domains\domain1\logs\*.log*
 echo=
 dir %PAYARA_HOME%\glassfish\domains\domain1\logs
+echo=
+dir .\logs
+echo=
+del .\logs\*.log*
+echo=
+dir .\logs
 
 pause
