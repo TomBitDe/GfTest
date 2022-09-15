ea6d37f3f6cf0d510530db6ffbd60a645f441d32
TomBitDe <tbit455674@gmail.com>
2022-09-15 18:11:53 +0200

    Documentation adapted.

--- ejb/src/main/java/com/home/gftest/jnditree/package-info.java
+++ ejb/src/main/java/com/home/gftest/jnditree/package-info.java
@@ -4,9 +4,9 @@
  * Sample REST calls:<br>
  * <br>
  * To fetch all entries<br>
- * - http://localhost:8080/war/ressources/jnditree/treeastext<br>
+ * - http://localhost:8080/war/ressources/jnditree/treeAsText<br>
  * <br>
  * To fetch all entries containing the keyword Ping<br>
- * - http://localhost:8080/war/ressources/jnditree/treeastext/Ping<br>
+ * - http://localhost:8080/war/ressources/jnditree/treeAsText/Ping<br>
  */
 package com.home.gftest.jnditree;
