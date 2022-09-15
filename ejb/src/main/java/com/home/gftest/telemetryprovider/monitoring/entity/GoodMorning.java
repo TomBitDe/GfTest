e9b0104807a38ce22ddd0a553191c2b6b5a9b3a2
TomBitDe <tbit455674@gmail.com>
2022-09-15 19:02:32 +0200

    Logging changed to log4j.

--- ejb/src/main/java/com/home/gftest/telemetryprovider/monitoring/entity/GoodMorning.java
+++ ejb/src/main/java/com/home/gftest/telemetryprovider/monitoring/entity/GoodMorning.java
@@ -1,25 +1,43 @@
 package com.home.gftest.telemetryprovider.monitoring.entity;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javax.ejb.Stateless;
 import javax.interceptor.Interceptors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.home.gftest.timed.TimerOtherSessionsBean;
+
+/**
+ * This is only a sample how to integrate the PerformanceAuditor.
+ */
 @Stateless
 @Interceptors(PerformanceAuditor.class)
 public class GoodMorning {
+	private static final Logger LOG = LogManager.getLogger(TimerOtherSessionsBean.class);
+	/**
+	 * Method say should have a duration of 200
+	 */
     public void say() {
+    	LOG.info("--> say()");
+
     	try {
 			Thread.sleep(200);
 		}
     	catch (Exception ex)
     	{
-			Logger.getLogger(GoodMorning.class.getName()).log(Level.SEVERE, null, ex);
+    		LOG.fatal(ex.getMessage());
 		}
+
+    	LOG.info("<-- say()");
     }
 
+    /**
+     * Method tooEarly throws an IllegalStateException
+     */
     public void tooEarly() {
+    	LOG.info("--> tooEarly()");
+
     	throw new IllegalStateException("Too early for good morning!");
     }
 }
